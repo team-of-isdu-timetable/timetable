d1590e437834a93f88ae8f4a0869e9ad1b271cd2
‘skanter’ <1790973909@qq.com>
2018-07-27 10:02:22 +0800

    Revert "php"
    This reverts commit dc06869ab47f0d98b2261b578fab932de28453ea.

--- php/school.php
+++ php/school.php
@@ -1,20 +1,7 @@
 <?php
 	error_reporting(0);
 	session_start();
-	$mysqli_con=mysqli_connect("localhost","isdu_timetable","cGOomDAMOPcJos9u&","isdu_timetable");
-
-	if (mysqli_connect_errno()) {
-		printf("Connect failed: %s\n", mysqli_connect_error());
-		exit();
-}
-	
-	$stmt = $mysqli_con->prepare("SELECT `custom` FROM `timetable` WHERE `id` = ?");
-	$stmt->bind_param("s",$_SESSION['id']);
-	$stmt->execute();
-	$stmt->bind_result($cus);
-	$stmt->fetch();
-	$stmt->close();
-	$custom=$cus;
+	$custom=$_SESSION['custom'];
 	$school=$_SESSION['school'];
 	$a=json_decode($school);
 	$b=json_decode($custom);
